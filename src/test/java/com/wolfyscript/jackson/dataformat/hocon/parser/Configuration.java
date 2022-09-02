98ccb8239953c354537f196a8f46826b0f78b499
WolfyScript <wolfyscript@gmail.com>
2022-09-03 00:03:20 +0200

    Improved Tests for arrays & added Collection tests

--- src/test/java/com/wolfyscript/jackson/dataformat/hocon/parser/Configuration.java
+++ src/test/java/com/wolfyscript/jackson/dataformat/hocon/parser/Configuration.java
@@ -1,6 +1,7 @@
 package com.wolfyscript.jackson.dataformat.hocon.parser;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class Configuration {
@@ -16,14 +17,40 @@ public class Configuration {
 			this.context = context;
 			this.value = value;
 		}
-		
-		public static class Context {
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Configuration that = (Configuration) o;
+		return Float.compare(that.value, value) == 0 && Objects.equals(something, that.something) && Objects.equals(context, that.context);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(something, context, value);
+	}
+
+	public static class Context {
 			public Lib lib;
 
 			public Context() { }
 
 			public Context(Lib lib) {
 				this.lib = lib;
+			}
+
+			@Override
+			public boolean equals(Object o) {
+				if (this == o) return true;
+				if (o == null || getClass() != o.getClass()) return false;
+				Context context = (Context) o;
+				return Objects.equals(lib, context.lib);
+			}
+
+			@Override
+			public int hashCode() {
+				return Objects.hash(lib);
 			}
 		}
 		
@@ -38,5 +65,17 @@ public class Configuration {
 				this.whatever = whatever;
 			}
 
+			@Override
+			public boolean equals(Object o) {
+				if (this == o) return true;
+				if (o == null || getClass() != o.getClass()) return false;
+				Lib lib = (Lib) o;
+				return Objects.equals(foo, lib.foo) && Objects.equals(whatever, lib.whatever);
+			}
+
+			@Override
+			public int hashCode() {
+				return Objects.hash(foo, whatever);
+			}
 		}
 }
--- src/test/java/com/wolfyscript/jackson/dataformat/hocon/parser/numerically_indexed_objs/NumericallyIndexedObjectsTest.java
+++ src/test/java/com/wolfyscript/jackson/dataformat/hocon/parser/numerically_indexed_objs/NumericallyIndexedObjectsTest.java
@@ -3,6 +3,7 @@ package com.wolfyscript.jackson.dataformat.hocon.parser.numerically_indexed_objs
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.wolfyscript.jackson.dataformat.hocon.HoconMapper;
+import com.wolfyscript.jackson.dataformat.hocon.parser.Configuration;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,8 +11,11 @@ import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -37,20 +41,20 @@ public class NumericallyIndexedObjectsTest {
     @Test
     public void testMultiElementInvalidValuesObject() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        Map<String, Object> stringArray = mapper.readValue(stream("multi-element-invalid-values.conf"), new TypeReference<Map<String, Object>>() {});
+        Map<String, Object> objectMap = mapper.readValue(stream("multi-element-invalid-values.conf"), new TypeReference<Map<String, Object>>() {});
         Map<String, Object> expected = new HashMap<>();
         expected.put("2", 0);
         expected.put("4", "f");
         expected.put("3", 0.7);
         expected.put("1", "test");
         expected.put("0", true);
-        Assert.assertEquals(expected, stringArray);
+        Assert.assertEquals(expected, objectMap);
     }
 
     @Test
     public void testMultiElementInvalidKeysObject() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        Map<String, Object> stringArray = mapper.readValue(stream("int-multi-element-invalid-keys.conf"), new TypeReference<Map<String, Object>>() {});
+        Map<String, Object> objectMap = mapper.readValue(stream("integer-invalid-keys.conf"), new TypeReference<Map<String, Object>>() {});
         Map<String, Object> expected = new HashMap<>();
         expected.put("bar", null);
         expected.put("4", 7);
@@ -58,108 +62,97 @@ public class NumericallyIndexedObjectsTest {
         expected.put("2", 5);
         expected.put("0", 9);
         expected.put("1", 8);
-        Assert.assertEquals(expected, stringArray);
+        Assert.assertEquals(expected, objectMap);
     }
 
     /*
     /**********************************************************
-    /* Conversions from Object to Array/Collection
-    /**********************************************************
-     */
-
-    /*
-    /**********************************************************
-    /* Boolean Arrays
+    /* Conversions from Object to Collection
     /**********************************************************
      */
 
     @Test
-    public void testBooleanSingleElement() throws IOException {
+    public void testSingleElementCollection() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        // Can use the int file here since the values are not above the max byte value
-        boolean[] stringArray = mapper.readValue(stream("boolean-single-element.conf"), boolean[].class);
-        Assert.assertArrayEquals(new boolean[]{ true }, stringArray);
+
+        assertCollection(mapper, "boolean-single-element.conf", new TypeReference<List<Boolean>>() {}, true);
+        assertCollection(mapper, "char-single-element.conf", new TypeReference<List<Character>>() {}, 'f');
+
+        // Integer values
+        assertCollection(mapper, "integer-single-element.conf", new TypeReference<List<Integer>>() {}, 9);
+        assertCollection(mapper, "integer-single-element.conf", new TypeReference<List<Long>>() {}, 9L);
+        assertCollection(mapper, "integer-single-element.conf", new TypeReference<List<Short>>() {}, (short) 9);
+        assertCollection(mapper, "integer-single-element.conf", new TypeReference<List<Byte>>() {}, (byte) 9);
+
+        // Floating point values
+        assertCollection(mapper, "float-single-element.conf", new TypeReference<List<Float>>() {}, 9.4f);
+        assertCollection(mapper, "float-single-element.conf", new TypeReference<List<Double>>() {}, 9.4d);
     }
 
-    @Test
-    public void testBooleanMultiElement() throws IOException {
-        HoconMapper mapper = new HoconMapper();
-        // Can use the int file here since the values are not above the max byte value
-        boolean[] stringArray = mapper.readValue(stream("boolean-multi-element.conf"), boolean[].class);
-        Assert.assertArrayEquals(new boolean[]{ true, false, false, true }, stringArray);
-    }
-
-    /*
-    /**********************************************************
-    /* Byte Arrays
-    /**********************************************************
-     */
-
-    @Test
-    public void testByteSingleElement() throws IOException {
-        HoconMapper mapper = new HoconMapper();
-        // Can use the int file here since the values are not above the max byte value
-        byte[] stringArray = mapper.readValue(stream("int-single-element.conf"), byte[].class);
-        Assert.assertArrayEquals(new byte[]{ 9 }, stringArray);
+    @SafeVarargs
+    private final <T> void assertCollection(HoconMapper mapper, String file, TypeReference<List<T>> typeReference, T... values) throws IOException {
+        // TypeReference is required as a parameter so that Jackson knows the actual type!
+        List<T> actual = mapper.readValue(stream(file), typeReference);
+        Assert.assertEquals(values.length, actual.size());
+        MatcherAssert.assertThat(actual, CoreMatchers.hasItems(values));
     }
 
     /*
     /**********************************************************
-    /* Integer Arrays
+    /* Conversions from Object to Array
     /**********************************************************
      */
 
     @Test
-    public void testIntSingleElement() throws IOException {
+    public void testBooleanArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        int[] stringArray = mapper.readValue(stream("int-single-element.conf"), int[].class);
-        Assert.assertArrayEquals(new int[]{ 9 }, stringArray);
+
+        Assert.assertArrayEquals(new boolean[]{ true }, mapper.readValue(stream("boolean-single-element.conf"), boolean[].class));
+        Assert.assertArrayEquals(new boolean[]{ true, false, false, true }, mapper.readValue(stream("boolean-multi-element.conf"), boolean[].class));
     }
 
     @Test
-    public void testIntMultiElement() throws IOException {
+    public void testByteArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        int[] stringArray = mapper.readValue(stream("int-multi-element.conf"), int[].class);
-        Assert.assertArrayEquals(new int[]{ 9, 8, 5, 7 }, stringArray);
+
+        Assert.assertArrayEquals(new byte[]{ 127 }, mapper.readValue("{ 0 = 127 }", byte[].class));
+        Assert.assertArrayEquals(new byte[]{ 127, 10, 120, 70 }, mapper.readValue("{ 0 = 127, 2 = 120, 1 = 10, 4 = 70 }", byte[].class));
+        Assert.assertArrayEquals(new byte[]{ 127, 10, 120, 70 }, mapper.readValue("{ 0 = 127, foo = bar, 2 = 120, bar = null, 1 = 10, 4 = 70 }", byte[].class));
+
+        Assert.assertThrows(InvalidFormatException.class, () -> mapper.readValue("{ 0 = true, 1 = test, 2 = 0, 3 = 0.7, 4 = f }", byte[].class));
     }
 
     @Test
-    public void testIntMultiElementInvalidKeys() throws IOException {
+    public void testIntArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        int[] stringArray = mapper.readValue(stream("int-multi-element-invalid-keys.conf"), int[].class);
-        Assert.assertArrayEquals(new int[]{ 9, 8, 5, 7 }, stringArray);
+
+        Assert.assertArrayEquals(new int[]{ Integer.MAX_VALUE }, mapper.readValue("{ 0 = 2147483647 }", int[].class));
+        Assert.assertArrayEquals(new int[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, 2 = 5, 1 = 8, 4 = 7 }", int[].class));
+        Assert.assertArrayEquals(new int[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, foo = bar, 2 = 5, bar = null, 1 = 8, 4 = 7 }", int[].class));
+
+        Assert.assertThrows(InvalidFormatException.class, () -> mapper.readValue("{ 0 = true, 1 = test, 2 = 0, 3 = 0.7, 4 = f }", int[].class));
     }
 
     @Test
-    public void testIntMultiElementInvalidValues() {
+    public void testShortArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        Assert.assertThrows(InvalidFormatException.class, () -> mapper.readValue(stream("multi-element-invalid-values.conf"), int[].class));
+
+        Assert.assertArrayEquals(new short[]{ Short.MAX_VALUE }, mapper.readValue("{ 0 = 32767 }", short[].class));
+        Assert.assertArrayEquals(new short[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, 2 = 5, 1 = 8, 4 = 7 }", short[].class));
+        Assert.assertArrayEquals(new short[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, foo = bar, 2 = 5, bar = null, 1 = 8, 4 = 7 }", short[].class));
+
+        Assert.assertThrows(InvalidFormatException.class, () -> mapper.readValue("{ 0 = true, 1 = test, 2 = 0, 3 = 0.7, 4 = f }", short[].class));
     }
 
-    /*
-    /**********************************************************
-    /* Short Arrays
-    /**********************************************************
-     */
-
     @Test
-    public void testShortSingleElement() throws IOException {
+    public void testLongArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        short[] stringArray = mapper.readValue(stream("short-single-element.conf"), short[].class);
-        Assert.assertArrayEquals(new short[]{ 9 }, stringArray);
-    }
 
-    /*
-    /**********************************************************
-    /* Long Arrays
-    /**********************************************************
-     */
+        Assert.assertArrayEquals(new long[]{ Long.MAX_VALUE }, mapper.readValue("{ 0 = 9223372036854775807 }", long[].class));
+        Assert.assertArrayEquals(new long[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, 2 = 5, 1 = 8, 4 = 7 }", long[].class));
+        Assert.assertArrayEquals(new long[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, foo = bar, 2 = 5, bar = null, 1 = 8, 4 = 7 }", long[].class));
 
-    @Test
-    public void testLongSingleElement() throws IOException {
-        HoconMapper mapper = new HoconMapper();
-        long[] stringArray = mapper.readValue(stream("long-single-element.conf"), long[].class);
-        Assert.assertArrayEquals(new long[]{ 9 }, stringArray);
+        Assert.assertThrows(InvalidFormatException.class, () -> mapper.readValue("{ 0 = true, 1 = test, 2 = 0, 3 = 0.7, 4 = f }", long[].class));
     }
 
     /*
@@ -169,10 +162,14 @@ public class NumericallyIndexedObjectsTest {
      */
 
     @Test
-    public void testFloatSingleElement() throws IOException {
+    public void testFloatArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        float[] stringArray = mapper.readValue(stream("float-single-element.conf"), float[].class);
-        Assert.assertArrayEquals(new float[]{ 9.4f }, stringArray, 0f);
+
+        Assert.assertArrayEquals(new float[]{ 9.43333f }, mapper.readValue("{ 0 = 9.43333 }", float[].class), 0f);
+        Assert.assertArrayEquals(new float[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, 2 = 5, 1 = 8, 4 = 7 }", float[].class), 0f);
+        Assert.assertArrayEquals(new float[]{ 9, 8.45f, 5, 7 }, mapper.readValue("{ 0 = 9, foo = bar, 2 = 5, bar = null, 1 = 8.45, 4 = 7 }", float[].class), 0f);
+
+        Assert.assertThrows(InvalidFormatException.class, () -> mapper.readValue("{ 0 = true, 1 = test, 2 = 0, 3 = 0.7, 4 = f }", float[].class));
     }
 
     /*
@@ -182,10 +179,14 @@ public class NumericallyIndexedObjectsTest {
      */
 
     @Test
-    public void testDoubleSingleElement() throws IOException {
+    public void testDoubleArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        double[] stringArray = mapper.readValue(stream("double-single-element.conf"), double[].class);
-        Assert.assertArrayEquals(new double[]{ 9.4d }, stringArray, 0f);
+
+        Assert.assertArrayEquals(new double[]{ 9.4d }, mapper.readValue("{ 0 = 9.4 }", double[].class), 0d);
+        Assert.assertArrayEquals(new double[]{ 9, 8, 5, 7 }, mapper.readValue("{ 0 = 9, 2 = 5, 1 = 8, 4 = 7 }", double[].class), 0d);
+        Assert.assertArrayEquals(new double[]{ 9, 8.45d, 5, 7 }, mapper.readValue("{ 0 = 9, foo = bar, 2 = 5, bar = null, 1 = 8.45, 4 = 7 }", double[].class), 0f);
+
+        Assert.assertThrows(InvalidFormatException.class, () -> mapper.readValue("{ 0 = true, 1 = test, 2 = 0, 3 = 0.7, 4 = f }", double[].class));
     }
 
     /*
@@ -195,17 +196,11 @@ public class NumericallyIndexedObjectsTest {
      */
 
     @Test
-    public void testCharSingleElement() throws IOException {
+    public void testCharArrays() throws IOException {
         HoconMapper mapper = new HoconMapper();
-        char[] stringArray = mapper.readValue(stream("char-single-element.conf"), char[].class);
-        Assert.assertArrayEquals(new char[]{ 'f' }, stringArray);
-    }
 
-    @Test
-    public void testCharMultiElement() throws IOException {
-        HoconMapper mapper = new HoconMapper();
-        char[] stringArray = mapper.readValue(stream("char-multi-element.conf"), char[].class);
-        Assert.assertArrayEquals(new char[]{ 'f', 'o', 'o', ',', 'b', 'a', 'r' }, stringArray);
+        Assert.assertArrayEquals(new char[]{ 'f' }, mapper.readValue(stream("char-single-element.conf"), char[].class));
+        Assert.assertArrayEquals(new char[]{ 'f', 'o', 'o', ',', 'b', 'a', 'r' }, mapper.readValue(stream("char-multi-element.conf"), char[].class));
     }
 
     /*
@@ -219,6 +214,22 @@ public class NumericallyIndexedObjectsTest {
         HoconMapper mapper = new HoconMapper();
         String[] stringArray = mapper.readValue(stream("string-single-element.conf"), String[].class);
         Assert.assertArrayEquals(new String[]{ "abc" }, stringArray);
+    }
+
+    /*
+    /**********************************************************
+    /* Object Arrays
+    /**********************************************************
+     */
+
+    @Test
+    public void testObjectSingleElement() throws IOException {
+        HoconMapper mapper = new HoconMapper();
+        Configuration[] configurations = mapper.readValue(stream("object-single-element.conf"), Configuration[].class);
+        Configuration[] expected = new Configuration[] {
+                new Configuration("something2", new Configuration.Context(new Configuration.Lib("foofoo", "randomString")), 6.9f)
+        };
+        Assert.assertArrayEquals(expected, configurations);
     }
 
 }
